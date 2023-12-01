50c4120e44116287736162430e439f293e9e1470
Hayden Gray <hayden.gray104@gmail.com>
2023-12-01 15:33:55 -0500

    final

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ const TILE_SIZE = 8
 function preload() {
   sprite_manager = new SpriteManager()
   let tiles = /** @type {Image[]} */ ([])
-  loadImage('/assets/cavernas.png', (img) => {
+  loadImage('/libraries/cavernas.png', (img) => {
     for (let y = 0; y < img.height; y += TILE_SIZE) {
       for (let x = 0; x < img.width; x += TILE_SIZE) {
         tiles.push(img.get(x, y, TILE_SIZE, TILE_SIZE))
@@ -20,72 +20,84 @@ function preload() {
   })
   sprite_manager.add_spritesheet('tilemap', tiles)
 
-  loadImage('/assets/player_idle.png', (img) => {
+  loadImage('/libraries/player_idle.png', (img) => {
     sprite_manager.add_sprite('player_idle', img, 10)
   })
-  loadImage('/assets/player_run.png', (img) => {
+  loadImage('/libraries/player_run.png', (img) => {
     sprite_manager.add_sprite('player_run', img, 8)
   })
-  loadImage('/assets/player_jump.png', (img) => {
+  loadImage('/libraries/player_jump.png', (img) => {
     sprite_manager.add_sprite('player_jump', img, 3)
   })
-  loadImage('/assets/player_fall.png', (img) => {
+  loadImage('/libraries/player_fall.png', (img) => {
     sprite_manager.add_sprite('player_fall', img, 6)
   })
-  loadImage('/assets/player_land.png', (img) => {
+  loadImage('/libraries/player_land.png', (img) => {
     sprite_manager.add_sprite('player_land', img, 9)
   })
-  loadImage('/assets/player_roll.png', (img) => {
+  loadImage('/libraries/player_roll.png', (img) => {
     sprite_manager.add_sprite('player_roll', img, 7)
   })
-  loadImage('/assets/player_jab.png', (img) => {
+  loadImage('/libraries/player_jab.png', (img) => {
     sprite_manager.add_sprite('player_jab', img, 8)
   })
-  loadImage('/assets/player_uppercut.png', (img) => {
+  loadImage('/libraries/player_uppercut.png', (img) => {
     sprite_manager.add_sprite('player_uppercut', img, 8)
   })
 
-  loadImage('/assets/Goblin_attack.png', (img) => {
+  loadImage('/libraries/Goblin_attack.png', (img) => {
     sprite_manager.add_sprite('Goblin_attack', img, 8)
   })
-  loadImage('/assets/Goblin_damaged.png', (img) => {
+  loadImage('/libraries/Goblin_damaged.png', (img) => {
     sprite_manager.add_sprite('Goblin_damaged', img, 4)
   })
-  loadImage('/assets/Goblin_death.png', (img) => {
+  loadImage('/libraries/Goblin_death.png', (img) => {
     sprite_manager.add_sprite('Goblin_death', img, 4)
   })
-  loadImage('/assets/Goblin_idle.png', (img) => {
+  loadImage('/libraries/Goblin_idle.png', (img) => {
     sprite_manager.add_sprite('Goblin_idle', img, 4)
   })
-  loadImage('/assets/Goblin_run.png', (img) => {
+  loadImage('/libraries/Goblin_run.png', (img) => {
     sprite_manager.add_sprite('Goblin_run', img, 8)
   })
 
-  loadImage('/assets/exit_door.png', (img) => {
+  loadImage('/libraries/exit_door.png', (img) => {
     sprite_manager.add_sprite('exit_door', img, 1)
   })
 
-  loadImage('/assets/title_card.png', (img) => {
+  loadImage('/libraries/title_card.png', (img) => {
     sprite_manager.add_sprite('title_card', img, 1)
   })
-  loadImage('/assets/play_button.png', (img) => {
+  loadImage('/libraries/play_button.png', (img) => {
     sprite_manager.add_sprite('play_button', img, 2)
   })
-  loadImage('/assets/tutor_button.png', (img) => {
+  loadImage('/libraries/tutor_button.png', (img) => {
     sprite_manager.add_sprite('tutor_button', img, 2)
   })
-  loadImage('/assets/back_button.png', (img) => {
+  loadImage('/libraries/back_button.png', (img) => {
     sprite_manager.add_sprite('back_button', img, 2)
   })
-  loadImage('/assets/tutorial_screen.png', (img) => {
+  loadImage('/libraries/tutorial_screen.png', (img) => {
     sprite_manager.add_sprite('tutorial_screen', img, 1)
+  })
+  loadImage('/libraries/restart_button.png', (img) => {
+    sprite_manager.add_sprite('restart_button', img, 2)
+  })
+  loadImage('/libraries/win_card.png', (img) => {
+    sprite_manager.add_sprite('win_card', img, 1)
+  })
+  loadImage('/libraries/lose_card.png', (img) => {
+    sprite_manager.add_sprite('lose_card', img, 1)
+  })
+  loadImage('/libraries/heart.png', (img) => {
+    sprite_manager.add_sprite('heart', img, 1)
   })
 
   let img = createGraphics(CANVAS_WIDTH + 10, CANVAS_HEIGHT + 10)
   img.background(BACKGROUND_COLOR)
   sprite_manager.add_sprite('background', img)
 
-  loaded_levels = /** @type {Level[]} */ (loadJSON('/assets/levels.json'))
+  loaded_levels = /** @type {Level[]} */ (loadJSON('/libraries/levels.json'))
 }
 
 function setup() {
